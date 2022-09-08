989b21dc6ca62ea1254e585cc77a1ce9ee115c68
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2022-09-08 23:05:35 +0700

    feat(setup): update eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,18 +7,26 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:import/recommended',
+    'plugin:@typescript-eslint/recommended',
     'plugin:jsdoc/recommended',
     'plugin:mocha/recommended',
     'plugin:prettier/recommended',
     'plugin:promise/recommended',
     'plugin:unicorn/recommended',
   ],
+  parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['import', 'jsdoc', 'mocha', 'prettier', 'promise', 'unicorn'],
+  plugins: [
+    '@typescript-eslint',
+    'jsdoc',
+    'mocha',
+    'prettier',
+    'promise',
+    'unicorn',
+  ],
   rules: {
     'jsdoc/require-jsdoc': [
       'error',
