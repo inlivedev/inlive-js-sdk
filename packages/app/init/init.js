638d882b8ff866cab24b6cbdaec4ef8132fc7f70
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2022-09-14 16:14:46 +0700

    feat: add more tests for init function
    - remove init.d.ts file
    - focus on using JSDoc for type checking and documentation

--- packages/app/init/init.js
+++ packages/app/init/init.js
@@ -1,25 +1,44 @@
-const baseOptions = {
-  apiKey: '',
-}
+/**
+ * @typedef Config
+ * @property {string | undefined} api_key - A string of key that will be used to access inLive protected API
+ */
 
 /**
- * @type {import('./init').init}
+ * @typedef InitializationObject
+ * @property {object} config - The key/value configuration inside the initialization object
+ * @property {string} config.api_key - A string of key that will be used to access inLive protected API
+ * @property {string} name - The name of initialization object instance
  */
-const init = (options = baseOptions) => {
-  return options && options.apiKey && options.apiKey.trim().length > 0
-    ? {
-        options: {
-          api_key: options.apiKey,
-        },
-        name: 'default',
-      }
-    : {
-        status: {
-          message:
-            'Failed to process because the API Key is not provided. Please provide an API Key.',
-          type: 'error',
-        },
-      }
+
+/**
+ * Initialize an initialization object
+ *
+ * @function
+ * @param {Config} config - A set of key/value parameter configuration
+ * @returns {InitializationObject} InitializationObject
+ * @throws {Error}
+ */
+const init = (config) => {
+  if (!config || config.api_key === undefined) {
+    throw new Error(
+      'Failed to process because the API key is not provided. Please provide an API key.'
+    )
+  } else if (typeof config.api_key !== 'string') {
+    throw new TypeError(
+      'Failed to process because the API key is not in a valid string format. API key must be in a string format'
+    )
+  } else if (config.api_key.trim().length === 0) {
+    throw new Error(
+      'Failed to process because the API key field is an empty string. Please provide an API key.'
+    )
+  } else {
+    return {
+      config: {
+        api_key: config.api_key,
+      },
+      name: 'default',
+    }
+  }
 }
 
 export { init }
