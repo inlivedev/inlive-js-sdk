df2f4dd545fad574cf50c807de953cc201ad5f41
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2022-09-14 19:48:57 +0700

    feat: add fetchHttp module
    - this module helps to interact with the api
    - need to add more test cases later

---
+++ packages/internal/fetch-http/fetch-http.js
@@ -0,0 +1,83 @@
+/**
+ * @typedef BaseConfig
+ * @property {string} url - The API endpoint URL
+ * @property {string} [token] - The bearer token for authorization purpose
+ * @property {'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'} [method] - The HTTP request methods used
+ * @property {object} [body] - The request body which contains the data that will be transmitted in an HTTP request
+ * @property {object} [options] - Option fetch parameter
+ */
+
+/**
+ * @type {BaseConfig}
+ */
+const baseConfig = {
+  url: '',
+  token: '',
+  method: 'GET',
+  body: undefined,
+  options: undefined,
+}
+
+/**
+ * A HTTP client module to fetch a request in the browser and server sides
+ *
+ * @function
+ * @param {BaseConfig} config A set of key/value parameter configuration
+ * @returns {Promise<any>} Returns a promise with the actual response data
+ * @throws {Error}
+ */
+export const fetchHttp = async (config = baseConfig) => {
+  const { url, token, method, body, options: fetchOptions } = config
+
+  if (url.trim().length === 0) {
+    throw new Error('Please provide the url')
+  }
+
+  const options = {
+    method: method || 'GET',
+    headers: {
+      Authorization: token ? `Bearer ${token}` : '',
+      'Content-Type': 'application/json',
+    },
+    body:
+      typeof body === 'object' && method !== 'GET'
+        ? JSON.stringify(body)
+        : undefined,
+    ...fetchOptions,
+  }
+
+  let fetchHttpRequest
+
+  if (typeof window === 'undefined' || !window) {
+    /**
+     * Server side fetch HTTP request
+     *
+     * @param {string} url - The API endpoint URL
+     * @param {object} options - Option fetch parameter
+     * @returns {Promise<any>} Returns a promise with the actual response data
+     */
+    fetchHttpRequest = (url, options) => {
+      return import('node-fetch').then(({ default: fetch }) => {
+        return fetch(url, options)
+      })
+    }
+  } else {
+    /**
+     * Client side fetch HTTP request
+     *
+     * @param {string} url - The API endpoint URL
+     * @param {object} options - Option fetch parameter
+     * @returns {Promise<any>} Returns a promise with the actual response data
+     */
+    fetchHttpRequest = (url, options) => {
+      return window.fetch(url, options)
+    }
+  }
+
+  const response = await fetchHttpRequest(url, options)
+  const json = await response.json()
+
+  return response.ok && json.code >= 200 && json.code < 300
+    ? Promise.resolve(json)
+    : Promise.reject(json)
+}
