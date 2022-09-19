4469bab8a3e1a20826d9bdda7ac6206899073130
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2022-09-19 18:58:39 +0700

    feat: create a config file for websocket

---
+++ packages/internal/config/websocket.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef WebSocket
+ * @property {string} base_url - The websocket base URL
+ * @property {string} version - The version of the webSocket API
+ */
+
+/**
+ * @type {WebSocket} The websocket configuration
+ */
+const websocket = {
+  base_url: 'wss://api.inlive.app',
+  version: 'v1',
+}
+
+Object.freeze(websocket)
+
+export { websocket }
