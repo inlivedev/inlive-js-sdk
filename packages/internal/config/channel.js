59a36eef2402e8e4381c976370d9471668718c50
Ingrid <ingrid.fortunata@asumsi.co>
2022-09-20 16:45:48 +0700

    Merge branch 'dev' into feat/create-stream

---
+++ packages/internal/config/channel.js
@@ -0,0 +1,15 @@
+/**
+ * @typedef Channel
+ * @property {string} base_url - The inLive stream channel base URL
+ */
+
+/**
+ * @type {Channel} - The channel configuration
+ */
+const channel = {
+  base_url: 'https://channel.inlive.app',
+}
+
+Object.freeze(channel)
+
+export { channel }
