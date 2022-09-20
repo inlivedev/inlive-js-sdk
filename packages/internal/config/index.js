cf0fdabd47c63e074a55a55ffb117e30ef71e7ac
Ingrid <ingrid.fortunata@asumsi.co>
2022-09-20 17:04:20 +0700

    Revert "Merge branch 'dev' into feat/create-stream"
    This reverts commit 59a36eef2402e8e4381c976370d9471668718c50.

--- packages/internal/config/index.js
+++
@@ -1,4 +0,0 @@
-export { api } from './api.js'
-export { channel } from './channel.js'
-export { webrtc } from './webrtc.js'
-export { websocket } from './websocket.js'
--- packages/internal/modules.js
+++ packages/internal/modules.js
@@ -1,2 +1,2 @@
 export { fetchHttp } from './fetch-http/fetch-http.js'
-export * as config from './config/index.js'
+export * as config from './config/config.js'
