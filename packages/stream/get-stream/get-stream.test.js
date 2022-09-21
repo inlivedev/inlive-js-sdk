ec5e4675a3ee1289b25470d100ad884171ee6ed5
Ingrid <ingrid.fortunata@asumsi.co>
2022-09-21 10:12:40 +0700

    add checking properties for get stream positive case mock data

--- packages/stream/get-stream/get-stream.test.js
+++ packages/stream/get-stream/get-stream.test.js
@@ -88,12 +88,20 @@ describe('Get Stream Module', function () {
             id: streamId,
             name: 'a new stream',
             slug: '',
-            hls_manifest_path: '',
-            dash_manifest_path: '',
+            start_time: '2022-09-06T02:11:39.954264Z',
+            end_time: '2022-09-06T02:38:50.746014Z',
+            billing_start: '2022-09-06T02:10:49.746014Z',
+            billing_end: '2022-09-06T02:38:50.746014Z',
+            prepared_at: '2022-09-06T02:10:49.746014Z',
+            hls_manifest_path:
+              'https://bifrost.inlive.app/streams/351/master.m3u8',
+            dash_manifest_path:
+              'https://bifrost.inlive.app/streams/351/manifest.mpd',
             description: '',
             created_by: 6,
             created_at: '2022-09-05T07:38:09.525329768Z',
-            updated_at: '2022-09-05T07:38:09.525329768Z',
+            updated_by: null,
+            updated_at: '2022-09-06T02:38:50.746014Z',
             quality: '360',
             viewer_count: 0,
           },
@@ -116,14 +124,35 @@ describe('Get Stream Module', function () {
       expect(result).to.have.property('status').to.be.an('object')
       expect(result).to.have.property('data').to.be.an('object')
       expect(result.data).to.have.property('id').to.be.equal(streamId)
-      expect(result.data).to.have.property('name').to.be.a('string')
+      expect(result.data).to.have.property('name').to.be.a('string').to.not.be
+        .empty
       expect(result.data).to.have.property('slug').to.be.a('string')
+      expect(result.data).to.have.property('description').to.be.a('string')
+      expect(result.data).to.have.property('start_time').to.be.a('string').to
+        .not.be.empty
+      expect(result.data).to.have.property('end_time').to.be.a('string').to.not
+        .be.empty
+      expect(result.data).to.have.property('billing_start').to.be.a('string').to
+        .not.be.empty
+      expect(result.data).to.have.property('billing_end').to.be.a('string').to
+        .not.be.empty
+      expect(result.data).to.have.property('billing_end').to.be.a('string').to
+        .not.be.empty
+      expect(result.data).to.have.property('prepared_at').to.be.a('string').to
+        .not.be.empty
       expect(result.data)
         .to.have.property('hls_manifest_path')
-        .to.be.a('string')
+        .to.be.a('string').to.not.be.empty
       expect(result.data)
         .to.have.property('dash_manifest_path')
-        .to.be.a('string')
+        .to.be.a('string').to.not.be.empty
+      expect(result.data).to.have.property('created_at').to.be.a('string').to
+        .not.be.empty
+      expect(result.data).to.have.property('updated_at').to.be.a('string').to
+        .not.be.empty
+      expect(result.data).to.have.property('quality').to.be.a('string').to.not
+        .be.empty
+      expect(result.data).to.have.property('viewer_count').to.be.a('number')
     })
   })
 })
