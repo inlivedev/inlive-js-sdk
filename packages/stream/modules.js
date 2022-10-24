15b21f9eb208558740a135b921cb9088cd22be81
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2022-10-24 09:16:25 +0700

    feat: a new stream module called connection module
    - this module manage the client through webrtc connection and currently using websocket connection

--- packages/stream/modules.js
+++ packages/stream/modules.js
@@ -1,6 +1,8 @@
+export { connection } from './connection/connection.js'
 export { createStream } from './create-stream/create-stream.js'
 export { endStream } from './end-stream/end-stream.js'
 export { getStreams } from './get-streams/get-streams.js'
 export { getStream } from './get-stream/get-stream.js'
+export { media } from './media/media.js'
 export { prepareStream } from './prepare-stream/prepare-stream.js'
 export { startStream } from './start-stream/start-stream.js'
