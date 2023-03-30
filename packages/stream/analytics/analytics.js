778943d4a78222c6b2f9cb2bddbc403bbd91b58a
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-03-30 13:52:30 +0700

    feat: use defined client ID

--- packages/stream/analytics/analytics.js
+++ packages/stream/analytics/analytics.js
@@ -1,7 +1,7 @@
 import { InitializationInstance } from '../../app/init/init.js'
 import { Internal } from '../../internal/index.js'
 import { api } from '../../internal/config/index.js'
-import merge from 'lodash-es/merge'
+import merge from 'lodash-es/merge.js'
 import FingerprintJS from '@fingerprintjs/fingerprintjs'
 import snakecaseKeys from 'snakecase-keys'
 import camelcaseKeys from 'camelcase-keys'
@@ -56,7 +56,7 @@ export const track = async (streamID, data, apiOptions) => {
   }
   const baseUrl = `${defaultConfig.baseUrl}/${defaultConfig.version}`
 
-  data.clientID = await getClientID()
+  data.clientID = data.clientID || (await getClientID())
   const statsAuthKey = await getStatsAuthKey(baseUrl, streamID, data.clientID)
 
   let fetchResponse = await Internal.fetchHttp({
