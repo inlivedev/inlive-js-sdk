7dba28a1d8058fd18d998eea081444b7c958a3c0
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-06 14:43:48 +0700

    feat: event module for room package

---
+++ packages/room/event/event.js
@@ -0,0 +1,62 @@
+export const createEvent = () => {
+  const Event = class {
+    /** @type {RoomEventType.EventItems} */
+    _events
+
+    constructor() {
+      this._events = {}
+    }
+
+    /**
+     * @param {string} eventName - The name of the event
+     * @param {SharedType.ObjectLiteral} [value] - The actual value sent
+     */
+    emit = (eventName, value = {}) => {
+      if (typeof eventName !== 'string' || eventName.trim().length === 0) {
+        throw new Error('Valid string for event name is required')
+      }
+
+      const data =
+        typeof value === 'object' && value !== null && !Array.isArray(value)
+          ? value
+          : { data: value }
+
+      const event = this._events[eventName]
+
+      if (event instanceof Set) {
+        for (const callback of event) {
+          typeof callback === 'function' && callback(data)
+        }
+      }
+    }
+
+    /**
+     * @param {string} eventName - The name of the event
+     * @param {(event: SharedType.ObjectLiteral) => void} callback - A callback function
+     */
+    on = (eventName, callback) => {
+      if (typeof eventName !== 'string' || eventName.trim().length === 0) {
+        throw new Error('Valid string for event name is required')
+      }
+
+      const event = this._events[eventName]
+
+      if (!(event instanceof Set)) {
+        this._events[eventName] = new Set()
+      }
+
+      this._events[eventName].add(callback)
+    }
+  }
+
+  return {
+    createInstance: () => {
+      const event = new Event()
+
+      return {
+        emit: event.emit,
+        on: event.on,
+      }
+    },
+  }
+}
