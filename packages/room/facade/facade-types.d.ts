69febfe0bc6ca948cff8637e92febc70fcd455e1
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-09-08 06:16:48 +0700

    refactor: remove missing part for reduce pr complexity

--- packages/room/facade/facade-types.d.ts
+++ packages/room/facade/facade-types.d.ts
@@ -1,9 +1,5 @@
 import type { RoomAPIType } from '../api/api-types.js'
-import type { RoomChannelType } from '../channel/channel-types.js'
-import type { RoomEventType } from '../event/event-types.js'
-import type { RoomPeerType } from '../peer/peer-types.js'
 import type { RoomType } from '../room-types.js'
-import type { RoomStreamType } from '../stream/stream-types.js'
 import type { createFacade } from './facade.js'
 
 export declare namespace RoomFacadeType {
@@ -16,23 +12,6 @@ export declare namespace RoomFacadeType {
     api: {
       createFetcher: RoomAPIType.CreateFetcher
       createApi: RoomAPIType.CreateApi
-    }
-    event: {
-      createEvent: RoomEventType.CreateEvent
-    }
-    stream: {
-      createStream: RoomStreamType.CreateStream
-      createStreams: RoomStreamType.CreateStreams
-    }
-    peer: {
-      createPeer: RoomPeerType.CreatePeer
-    }
-    channel: {
-      createChannel: RoomChannelType.CreateChannel
-    }
-    roomEvents: {
-      peer: RoomPeerType.PeerEvents
-      channel: RoomChannelType.ChannelEvents
     }
   }
 }
