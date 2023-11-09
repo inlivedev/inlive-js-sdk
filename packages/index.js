5b858a04807bb95eb416fd8a4b24629d691a25c6
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2023-11-09 13:47:18 +0700

    feat: export RoomEvent

--- packages/index.js
+++ packages/index.js
@@ -1,3 +1,3 @@
 export { InliveApp } from './app/index.js'
 export { InliveStream, Stream, Stat } from './stream/index.js'
-export { Room } from './room/index.js'
+export { Room, RoomEvent } from './room/index.js'
