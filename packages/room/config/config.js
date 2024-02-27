6e212c74ce01b687a984373dd984d078e8185cd0
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-02-27 20:46:56 +0700

    feat: add webcam and screen configuration

--- packages/room/config/config.js
+++ packages/room/config/config.js
@@ -20,16 +20,39 @@ export const webrtc = {
 }
 
 export const media = {
-  video: {
+  webcam: {
     width: 1280,
     height: 720,
     frameRate: 24,
+    maxFramerate: 30,
     facingMode: 'user',
     aspectRatio: 1.777_777_777_8,
-    maxBitrate: 1_200_000,
-    codec: 'VP9',
+    codecs: ['video/VP9', 'video/VP8', 'video/H264'],
+    simulcast: false,
+    bitrate: {
+      highBitrate: 1200 * 1000,
+      midBitrate: 500 * 1000,
+      lowBitrate: 150 * 1000,
+    },
+    scalabilityMode: 'L3T1',
+  },
+  screen: {
+    width: 1280,
+    height: 720,
+    frameRate: 24,
+    maxFramerate: 30,
+    aspectRatio: 1.777_777_777_8,
+    codecs: ['video/VP9', 'video/VP8', 'video/H264'],
+    simulcast: false,
+    bitrate: {
+      highBitrate: 1200 * 1000,
+      midBitrate: 500 * 1000,
+      lowBitrate: 150 * 1000,
+    },
+    scalabilityMode: 'L3T1',
   },
   audio: {
+    codecs: ['audio/red', 'audio/opus'],
     echoCancellation: true,
     noiseSuppression: true,
     autoGainControl: true,
