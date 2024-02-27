f801bfaacdd31515da2e82eb6492eb939a260190
Faiq Naufal <43695578+faiq-naufal@users.noreply.github.com>
2024-02-27 20:47:57 +0700

    refactor: config type reference from config module

--- packages/room/room-types.d.ts
+++ packages/room/room-types.d.ts
@@ -1,42 +1,16 @@
+import { api, webrtc, media } from './config/config.js'
+
 export declare namespace RoomType {
   type Config = {
-    api: {
-      baseUrl: string
-      version: string
-      apiKey: string
-    }
-    webrtc: {
-      iceServers: RTCIceServer[]
-    }
-
-    media: {
-      video: {
-        width: number
-        height: number
-        frameRate: number
-        facingMode: string
-        aspectRatio: number
-        maxBitrate: number
-        codec: string
-      }
-      audio: {
-        echoCancellation: boolean
-        noiseSuppression: boolean
-        autoGainControl: boolean
-        red: boolean
-      }
-    }
+    api: typeof api
+    webrtc: typeof webrtc
+    media: typeof media
   }
 
   type UserConfig = {
-    api?: {
-      baseUrl?: string
-      version?: string
-      apiKey?: string
-    }
-    webrtc?: {
-      iceServers?: RTCIceServer[]
-    }
+    api?: Partial<typeof api>
+    webrtc?: Partial<typeof webrtc>
+    media?: Partial<typeof media>
   }
 
   type BitrateConfigs = {
